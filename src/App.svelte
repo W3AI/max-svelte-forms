548537d99b00de3c25d492aaf9847fac92aa9749
stefian <v_ianta@yahoo.com>
2019-05-21 12:15:45 -0400

    avoid 2way binding everywhere

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,10 @@
     import Toggle from './Toggle.svelte';
 
     let val = 'Stef';
+    let selectedOption =1;
 
     $: console.log(val);
+    $: console.log(selectedOption);
 
     function setValue(event) {
         val = event.target.value;
@@ -15,4 +17,6 @@
 <!-- <input type="text" bind:value={val}> -->
 <CustomInput type="text" bind:val={val} />
 
-<Toggle />
+<!-- Avoid 2 way binding everywhere except forms and the like -->
+<!-- To avoid introducing errors / complex behaviors, etc -->
+<Toggle bind:chosenOption={selectedOption} />
